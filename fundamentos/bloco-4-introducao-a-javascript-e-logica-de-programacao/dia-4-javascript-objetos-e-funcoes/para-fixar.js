c0f548ca1c912e4d6d84d07589cd220903fe0fbd
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2022-05-17 16:10:56 -0300

    para fixar - dia 4.4 - for/in e for/of

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/para-fixar.js
@@ -1,13 +1,19 @@
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: { 
-    golden: 2, silver: 3 
-  },
-  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let key in names) {
+  console.log ("Olá, " + names[key]);
 }
 
-console.log ("a jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
-console.log ("a jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo " + player.bestInTheWorld.length + " vezes.");
-console.log ("a jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.");
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log (key, car[key]);
+}
