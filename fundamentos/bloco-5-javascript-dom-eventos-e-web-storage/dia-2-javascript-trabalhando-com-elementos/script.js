ccb6b7bbf46438b3c078ee1fa2c57167abeaacfc
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2022-05-20 18:44:55 -0300

    questao 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,3 +1,8 @@
 let textoH1 = document.createElement('h1');
 textoH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(textoH1);
+
+let tagMain = document.createElement('main');
+tagMain.className = 'main-content';
+tagMain.innerText = 'questao 2';
+document.body.appendChild(tagMain);
